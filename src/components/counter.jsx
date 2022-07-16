43dcb1d734161a5c4e70bdd17dbaaf54032b4e48
NechipurenkoDmitriy <pikinaplecho@gmail.com>
2022-07-16 21:29:53 +0300

    First Commit Lesson2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,61 +1,46 @@
 import React, { useState } from 'react';
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-  const [tags, setTags] = useState(['tag1', 'tag2', 'tag3']);
+const Counter = (props) => {
+  const [value, setValue] = useState(props.value);
 
-  const formatCount = () => {
-    return count === 0 ? 'empty' : count;
+  const formatValue = () => {
+    return value === 0 ? 'empty' : value;
   };
 
   const getBadgeClasses = () => {
     let classes = 'badge m-2 ';
-    classes += count === 0 ? 'bg-warning' : 'bg-primary';
+    classes += value === 0 ? 'bg-warning' : 'bg-primary';
     return classes;
   };
 
   const handleIncrement = () => {
-    //setCount(count + 1);
-    setCount((prevState) => prevState + 1);
-    //setCount((prevState) => prevState + 1);
+    //setValue(count + 1);
+    setValue((prevState) => prevState + 1);
+    //setValue((prevState) => prevState + 1);
     //console.log(count);
   };
 
   const handleDecrement = () => {
-    setCount((prevState) => prevState - 1);
+    setValue((prevState) => prevState - 1);
   };
-
-  const handleTagChange = (id) => {
-    setTags((prevState) => prevState.filter((tag) => tag !== id));
-  };
-
-  const renderTags = () => {
-    if (tags.length === 0) return 'No tags';
-    return tags.map((tag) => (
-      <li
-        key={tag}
-        className="btn btn-primary btn-sm m-2 "
-        onClick={() => handleTagChange(tag)}
-      >
-        {tag}
-      </li>
-    ));
-  };
-
-  if (tags.length !== 0) {
-    return <ul>{renderTags()}</ul>;
-  }
 
   return (
-    <>
-      <span className={getBadgeClasses()}>{formatCount()}</span>
+    <div>
+      <span>{props.name}</span>
+      <span className={getBadgeClasses()}>{formatValue()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
       </button>
       <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
         -
       </button>
-    </>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => props.onDelete(props.id)}
+      >
+        Delete
+      </button>
+    </div>
   );
 };
 
