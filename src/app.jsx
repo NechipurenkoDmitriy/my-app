4f9c494c7d7d950e000f38a55e36699dcd4a7beb
NechipurenkoDmitriy <pikinaplecho@gmail.com>
2022-07-16 21:30:29 +0300

    Commit

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+//import Counter from './components/counter';
+import CounterList from './components/countersList';
+
+const App = () => {
+  return <CounterList />;
+};
+
+export default App;
