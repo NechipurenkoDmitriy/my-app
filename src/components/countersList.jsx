67db3ab70e81808c494978fd8e4eb726e681848b
NechipurenkoDmitriy <pikinaplecho@gmail.com>
2022-07-16 23:01:00 +0300

    HW is working

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -12,6 +12,26 @@ const CounterList = () => {
 
   const [counters, setCounters] = useState(initialState);
 
+  const handleIncrement = (id) => {
+    const newCounters = [...counters];
+    newCounters.forEach((c) => {
+      if (c.id === id) {
+        c.value += 1;
+      }
+    });
+    setCounters(newCounters);
+  };
+
+  const handleDecrement = (id) => {
+    const newCounters = [...counters];
+    newCounters.forEach((c) => {
+      if (c.id === id) {
+        c.value -= 1;
+      }
+    });
+    setCounters(newCounters);
+  };
+
   const handleDelete = (id) => {
     const newCounters = counters.filter((c) => c.id !== id);
     setCounters(newCounters);
@@ -20,27 +40,20 @@ const CounterList = () => {
   const handleReset = () => {
     setCounters(initialState);
   };
-  const handleUpdate = () => {
-    const updatedState = [
-      { id: 0, value: 1, name: 'Ненужная вещь', price: '200' },
-      { id: 1, value: 2, name: 'Ложка' },
-      { id: 2, value: 3, name: 'Вилка' },
-      { id: 3, value: 4, name: 'Тарелка' },
-      { id: 4, value: 0, name: 'Набор минималиста' },
-    ];
-    setCounters(updatedState);
-  };
 
   return (
     <>
       {counters.map((count) => (
-        <Counter key={count.id} onDelete={handleDelete} {...count} />
+        <Counter
+          key={count.id}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          onDelete={handleDelete}
+          {...count}
+        />
       ))}
       <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>
         Сброс
-      </button>
-      <button className="btn btn-primary btn-sm m-2" onClick={handleUpdate}>
-        Обновить состояние
       </button>
     </>
   );
